25179d20e023fc9e24582b48f14d009bca91984c
dwqs <461147874@qq.com>
2017-11-27 01:30:04 +0800

    更新导航样式

--- docs/components/side-nav.vue
+++ docs/components/side-nav.vue
@@ -41,11 +41,30 @@
 <style lang="less">
     .docs-side-nav {
         position: fixed;
-        width: 220px;
+        width: 175px;
         height: auto;
         padding: 15px;
         border: 1px solid #edeef2;
         background: #fff;
         vertical-align: top;
+        .nav-item {
+            // height: 36px;
+            line-height: 36px;
+            a {
+                color: #333;
+                &.active {
+                    color: #ff6200;
+                }
+                &:hover {
+                    color: #ff6200;
+                }
+            }
+            .sub-nav li{
+                margin-left: 10px;
+            }
+            .sub-nav .sub-nav-title {
+                color: #999;
+            }
+        }
     }
 </style>
