2b6187bc3c15fc235d45a91bd8ae82b057796612
dwqs <461147874@qq.com>
2017-11-30 12:34:05 +0800

    karma 配置

---
+++ build/webpack.test.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: path.join(__dirname, '../src/index'),
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.less$/,
+                use: ['vue-style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            }
+        ]
+    },
+    // externals: config.externals,
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            components: path.resolve(__dirname, '../components'),
+            'main': path.resolve(__dirname, '../src'),
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        modules: [path.join(__dirname, '../node_modules')]
+    },
+    devtool: 'inline-source-map'
+};
