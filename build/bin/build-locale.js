4deff5cddf8ad3f05a14c92b89c49333e8916e22
dwqs <461147874@qq.com>
2017-12-02 00:15:18 +0800

    build locale

---
+++ build/bin/build-locale.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').execSync;
+
+const i18nPath = path.resolve(__dirname, '../../src/locale/i18n.js');
+const outputPath = path.resolve(__dirname, '../../lib/locale/i18n.js');
+
+function dirExist (path) {
+    try {
+        fs.accessSync(path, fs.F_OK);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+function writeFile (err, result) {
+    if (err) {
+        console.log('write failed', err);
+    } else {
+        if (!dirExist(path.resolve(__dirname, '../../lib'))) {
+            exec('mkdir lib');
+        }
+
+        if (!dirExist(path.resolve(__dirname, '../../lib/locale'))) {
+            exec('mkdir lib/locale');
+        }
+
+        exec('cp -rf ./src/locale/ ./lib/locale/');
+        const code = result.code;
+        fs.writeFileSync(outputPath, code);
+    }
+}
+
+require('babel-core').transformFile(i18nPath, {
+    plugins: ['transform-es2015-modules-umd'],
+    moduleId: 'i18n'
+}, writeFile);
