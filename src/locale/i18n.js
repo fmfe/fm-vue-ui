79a99559263055f35a4f861666057059dfd5ca49
dwqs <461147874@qq.com>
2017-12-02 21:19:22 +0800

    更改 i18n 的支持方式

--- src/locale/i18n.js
+++ src/locale/i18n.js
@@ -3,23 +3,18 @@ import cn from './lang/zh-CN.json';
 import en from './lang/en-US.json';
 
 const supprtsLangs = ['zh-CN', 'en-US'];
-let vueI18n;
+window.vueI18n = undefined;
 
 function setI18n (i18n) {
-    const lang = supprtsLangs.includes(i18n.locale) ? i18n.locale : 'zh-CN';
+    const lang = supprtsLangs.indexOf(i18n.locale) > -1 ? i18n.locale : 'zh-CN';
     if (i18n.vm) {
         i18n.mergeLocaleMessage(lang, lang === 'zh-CN' ? cn : en);
-        vueI18n = i18n;
+        window.__vueI18n = i18n;
     } else {
         throw new Error('The instance of vue-i18n must be needed.');
     }
 };
 
-function getI18n (i18n) {
-    return vueI18n;
-};
-
 export default {
-    setI18n,
-    getI18n
+    setI18n
 };
