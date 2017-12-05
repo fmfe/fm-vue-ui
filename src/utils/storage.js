a2cae6cc93f78bff095f19cdeba6c7f3fd014f96
dwqs <461147874@qq.com>
2017-12-05 10:30:35 +0800

    封装Storage操作

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,8 +1,56 @@
 const prefix = '_fm_';
-export const localStorage = {
+const { localStorage: l, sessionStorage: s } = window;
 
+export const localStorage = {
+    set (key, val) {
+        try {
+            l.setItem(prefix + key, JSON.stringify(val));
+        } catch (err) {
+            alert('localStorage 写入出错');
+        }
+    },
+
+    get (key) {
+        try {
+            const val = l.getItem(prefix + key);
+            return JSON.parse(val);
+        } catch (e) {
+            return l.getItem(prefix + key);
+        }
+    },
+
+    remove (key) {
+        l.removeItem(prefix + key);
+    },
+
+    clear () {
+        l.clear();
+    }
 };
 
 export const sessionStorage = {
+    set (key, val) {
+        try {
+            s.setItem(prefix + item, JSON.stringify(val));
+        } catch (err) {
+            alert('sessionStorage 写入出错');
+        }
+    },
 
+    get (key) {
+        try {
+            const val = s.getItem(prefix + key);
+            return JSON.parse(val);
+        } catch (e) {
+            return s.getItem(prefix + key);
+        }
+    },
+
+    remove (key) {
+        s.removeItem(prefix + key);
+    },
+
+    clear () {
+        s.clear();
+    }
 };
